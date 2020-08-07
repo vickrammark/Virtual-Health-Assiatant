cb45186aabd41b2830c4347f378a14d21ace96ab
DELL <vickramark54@gmail.com>
2020-08-07 21:30:03 +0530

    Updating some features

---
+++ src/main/java/com/VirutsaProject/virtual_health_assistant/Setter/updaterSetter.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.VirutsaProject.virtual_health_assistant.Setter;
+
+/**
+ *
+ * @author DELL
+ */
+public class updaterSetter {
+     private String presId;
+     private String AppDate;
+     private String AppTime;
+     private String AppConfirmation;
+
+    public String getAppDate() {
+        return AppDate;
+    }
+
+    public void setAppDate(String AppDate) {
+        this.AppDate = AppDate;
+    }
+
+    public String getAppTime() {
+        return AppTime;
+    }
+
+    public void setAppTime(String AppTime) {
+        this.AppTime = AppTime;
+    }
+
+    public String getAppConfirmation() {
+        return AppConfirmation;
+    }
+
+    public void setAppConfirmation(String AppConfirmation) {
+        this.AppConfirmation = AppConfirmation;
+    }
+
+    public String getPresId() {
+        return presId;
+    }
+
+    public void setPresId(String presId) {
+        this.presId = presId;
+    }
+}
